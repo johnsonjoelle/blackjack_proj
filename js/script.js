d0d42d04ae2896cf8c6ea4fd99ce71215a5b83d5
Joelle Johnson <joelle.johnson@dcmail.ca>
2022-07-14 14:35:01 -0400

    Card Shuffle and Addition Implemented
    Cards can be shuffled using the shuffle button. Clicking the draw button alternates between the dealer and player, drawing cards for each on each button press and adding to their totals. 21 Limit is not yet implemented.

--- js/script.js
+++ js/script.js
@@ -11,10 +11,20 @@
 
 // * Global Variables
 let cards = [];
+let deck = [];
+let shuffledDeck;
 // const hand = document.getElementById('hand');
 const dealer = document.getElementById('dealer');
 const drawBtn = document.getElementById('draw-btn');
+const startBtn = document.getElementById('start-btn');
 let deckPos = 0;
+let playerCardTotal = 0;
+let dealerCardTotal = 0;
+const cardLimit = 21;
+const playerTurn = 'player';
+const dealerTurn = 'dealer';
+let turn = playerTurn;
+let tempCardVal;
 
 // * Create the Card Array
 function createCardArray() {
@@ -74,15 +84,19 @@ function createCardFace(cardData) {
     if (cardData.suite=='spade' || cardData.suite=='club') {
       color = 'black';
     }
+    let num;
     switch (cardData.num) {
       case 'J':
         royal = 'jack';
+        num = 11;
         break;
       case 'Q':
         royal = 'queen';
+        num = 12;
         break;
       case 'K':
         royal = 'king';
+        num = 13;
         break;
     
       default:
@@ -92,7 +106,7 @@ function createCardFace(cardData) {
 
     centerContent = `<div class="pip"><img src="img/${royal}_${color}.svg"></div>`;
     cardFace = `
-      <article class="card" data-card="${cardData.num}" data-suite="${cardData.suite}">
+      <article class="card" data-card="${cardData.num}" data-suite="${cardData.suite}" data-value="${num}">
         <div class="identifier top">
           <b class="key">${cardData.num}</b>
           <img class="type" src="img/${cardData.suite}.svg">
@@ -105,6 +119,7 @@ function createCardFace(cardData) {
           <img class="type" src="img/${cardData.suite}.svg">
         </div>
       </article>`;
+    tempCardVal = num;
   } else {
     let num = 1;
     if (!isNaN(cardData.num)) {
@@ -114,7 +129,7 @@ function createCardFace(cardData) {
       centerContent += `<div class="pip"><img src="img/${cardData.suite}.svg"></div>`;
     }
     cardFace = `
-      <article class="card" data-card="${cardData.num}" data-suite="${cardData.suite}">
+      <article class="card" data-card="${cardData.num}" data-suite="${cardData.suite}" data-value="${num}">
         <div class="identifier top">
           <b class="key">${cardData.num}</b>
           <img class="type hidden" src="img/${cardData.suite}.svg">
@@ -127,19 +142,74 @@ function createCardFace(cardData) {
           <img class="type hidden" src="img/${cardData.suite}.svg">
         </div>
       </article>`;
+    tempCardVal = num;
   }
   return cardFace;
 }
 
+// * Alternate Turns
+function changeTurns(currentTurn) {
+  if (currentTurn == playerTurn) {
+    turn = dealerTurn;
+  } else {
+    turn = playerTurn;
+  }
+  // console.log(turn);
+}
+
 // * Draw A Card
 function drawCard() {
-  const cardFace = createCardFace(cards[deckPos]);
+  const cardFace = createCardFace(deck[deckPos]);
   deckPos += 1;
-  $('#hand').append(cardFace);
+  if (turn == 'player') {
+    $('#hand').append(cardFace);
+  } else {
+    $('#dealer').append(cardFace);
+  }
+  
+  sumCards()
+}
+
+// * Sum Cards
+function sumCards() {
+  if (turn == 'player') {
+    playerCardTotal += tempCardVal;
+    $('.player-count').text(playerCardTotal);
+  } else {
+    dealerCardTotal += tempCardVal;
+    $('.dealer-count').text(dealerCardTotal);
+  }
+  changeTurns(turn);
+}
+
+// * Shuffle Deck
+function shuffleDeck() {
+  deck = [...cards]
+  let currentIndex = deck.length;
+  let tempValue;
+  let randomIndex;
+
+  while(currentIndex>0) {
+    randomIndex = Math.floor(Math.random()*currentIndex);
+    currentIndex -=1;
+    tempValue = deck[currentIndex];
+    deck[currentIndex] = deck[randomIndex];
+    deck[randomIndex] = tempValue;
+    // console.log(`${deck[currentIndex]} switched with ${deck[randomIndex]}`);
+  }
+  return deck;
 }
 
 function init() {
   createCardArray();
+
+  startBtn.addEventListener('click', () => {
+    // Reset game board
+    // Shuffle deck
+    // Unlock other buttons
+    shuffleDeck();
+    // console.log(deck);
+  });
 
   drawBtn.addEventListener('click', () => {
     drawCard();
